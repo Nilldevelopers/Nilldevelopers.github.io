4a6f7ee9b4a735af8d91b4c21fd26831cb76957b
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-04-26 15:09:17 +0430

    PWA release!

---
+++ sw.js
@@ -0,0 +1,52 @@
+const cacheName = "Nill-v1";
+const staticAssets = [
+  "./",
+  "./index.html",
+  "./css/plugins.css",
+  "./css/style.css",
+  "./css/costume.css",
+  "./js/jquery-3.1.1.min.js",
+  "./js/plugins.js",
+  "./js/dsn-grid.js",
+  "./js/custom.js",
+  "./js/iconify.js"
+];
+
+self.addEventListener("install", async (e) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", async (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
